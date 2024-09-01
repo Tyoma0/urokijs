b07476f16ab48432c6060c0c61a6c618c05c9e5e
Tyoma0 <artyom0070071@mail.ru>
2024-09-01 11:11:39 +0300

    d

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,12 +1,7 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
-clear ==true;
-odd == false;
-function isEven(clear, odd) {
-   if (clear) {
-    console.log(четное);
-   } if (odd ==false) {
-    console.log(нечетное);
-   }
+
+function isEven(num) {
+    return num % 2 === 0;
 }
 isEven(clear,odd)
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,7 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
+let n1 = 1;
+let n2 =2;
+let n3 =3;
 
-function findLargest() {}
+
+function findLargest(n1,n2,n3) {}
