6f7b256821d230c09a8884c1978f690244117ee9
Tyoma0 <artyom0070071@mail.ru>
2024-09-12 20:38:00 +0300

    h

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,15 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(galleryfat,artwork, artistInfo) {
+ 
+  if (galleryfat.hasOwnProperty(artwork)) {
+    galleryfat[artwork] = artistInfo;
+  } else{
+    galleryObj[artwork] = artistInfo;
+  }
+}
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506');
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali');
+
+console.log(gallery);
